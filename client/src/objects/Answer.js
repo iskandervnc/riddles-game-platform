39d2a04f4a0025b962f0dde0cb3605abe169110e
Alessandro Vancini <100768578+IskanderVnc@users.noreply.github.com>
2025-11-19 15:20:48 +0100

    Delete client directory

--- client/src/objects/Answer.js
+++
@@ -1,8 +0,0 @@
-function Answer(riddleID, answer, author, submissionDate) {
-    this.riddleID = riddleID;
-    this.answer = answer;
-    this.author = author;
-    this.submissionDate = submissionDate;
-}
-
-exports.Answer = Answer;
