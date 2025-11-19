9613c4aa505a5749608a00d809bddb34792a5b4e
Alessandro Vancini <100768578+IskanderVnc@users.noreply.github.com>
2025-11-19 15:21:10 +0100

    Delete server directory

--- server/database/db.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const sqlite = require('sqlite3');
-
-exports.db = new sqlite.Database('./database/db.sqlite', (err) => {
-    if (err) throw err;
-})
--- server/objects/User.js
+++
@@ -1,7 +0,0 @@
-function User(username, score, rank = undefined) {
-    this.username = username;
-    this.score = score;
-    this.rank = rank;
-}
-
-exports.User = User;
